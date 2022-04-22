47f53d35ee885fb8faffab0150bf9312a7c59a7d
rluver <skyd0608@gmail.com>
2022-04-22 22:00:27 +0900

    add InputAttention

--- tensorflow/layers/encoders.py
+++ tensorflow/layers/encoders.py
@@ -1,4 +1,5 @@
-from tensorflow.keras.layers import Input, LSTM, Layer, Dense
+import xdrlib
+from tensorflow.keras.layers import Input, LSTM, Layer, Dense, RepeatVector
 from tensorflow.keras.models import Model
 
 
@@ -15,3 +16,28 @@ class NewLSTM(LSTM):
     
     def reset_state(self, hidden_state, cell_state):
         self.initial_state = [hidden_state, cell_state]
+
+
+class InputAttention(Model):
+    def __init__(self, units, **kwargs):
+        super(InputAttention, self).__init__(**kwargs)
+        self.w1 = Dense(units)
+        self.w2 = Dense(units)
+        self.v = Dense(1)
+
+    def call(self, hidden_state, cell_state, x):
+        query = tf.concat([hidden_state, cell_state], axis=-1)
+        query = RepeatVector(x.shape[2])(query)
+        
+        x_permuted = Permute((2, 1))(x)
+
+        score = tf.nn.tanh(self.w1(x_permuted) + self.w2(query))
+        score = self.v(score)
+        score = Permute((2, 1))(score)
+
+        attention_weights = tf.nn.softmax(score, axis=-1)
+
+        context_vector = attention_weights * x
+        context_vector = tf.reduce_sum(context_vector, axis=-1)
+
+        return context_vector, attention_weights
