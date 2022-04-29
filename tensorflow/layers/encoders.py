0cd7a1d1952a5e408dfca9d29a6406f442711078
rluver <skyd0608@gmail.com>
2022-04-29 11:57:32 +0900

    feat: add Decoder

--- tensorflow/layers/encoders.py
+++ tensorflow/layers/encoders.py
@@ -81,7 +81,7 @@ class TemporalAttention(Layer):
     def call(self, hidden_state, cell_state, encoder_hidden_state):
         query = tf.concat([hidden_state, cell_state], axis=-1)
         query = RepeatVector(encoder_hidden_state.shape[1])(query)
-        
+
         score = tf.nn.tanh(self.w1(encoder_hidden_state) + self.w2(query))
         score = self.v(score)
 
@@ -89,5 +89,33 @@ class TemporalAttention(Layer):
 
         return attention_weights
 
-    
-    
+
+class Decoder(Layer):
+    def __init__(self, units, seq_len, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+        self.seq_len = seq_len
+        self.temporal_attention = TemporalAttention(units)
+        self.lstm = NewLSTM(units)
+        self.dense = Dense(1)
+        self.encoder_dim = kwargs.get('encoder_dim')
+        self.decoder_dim = kwargs.get('decoder_dim')
+        self.context_vector
+        self.b_t = None
+
+    def call(self, x, hidden_state, cell_state, encoder_hidden_state, training=False):
+        self.lstm.reset_state(hidden_state=hidden_state, cell_state=cell_state)
+        context_vector = tf.zeros((encoder_hidden_state.shape[0], 1, self.encoder_dim))
+        decoder_hidden_state = tf.zeros(encoder_hidden_state.shape[0], self.decoder_dim)
+
+        for t in range(self.seq_len - 1):
+            l = Lambda(lambda x: x[:, t, :])(x)
+            l = l[:, tf.newaxis, :]
+            l = tf.concat([l, context_vector], axis=-1)
+            l = self.dense(l)
+
+            hidden_state, cell_state = self.lstm(l)
+            self.b_t = self.temporal_attention(hidden_state, cell_state, encoder_hidden_state)
+            context_vector = tf.matmul(self.b_t, encoder_hidden_state, transpose_a=True)
+
+        output = tf.concat([hidden_state[:, tf.newaxis, :], self.context_vector], axis=-1)
+        return output
